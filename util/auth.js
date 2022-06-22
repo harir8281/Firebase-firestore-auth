7e54268b5e62ebdc397487ca63c07e94b74c12f6
Harikrishnan.r <harir3737@gmail.com>
2022-06-22 18:02:44 +0530

    intial commit

--- util/auth.js
+++ util/auth.js
@@ -18,6 +18,8 @@ async function authenticate(mode, email, password) {
     await authenticate('signUp', email, password);
   }
   
+
+  
   export async function login(email, password) {
     await authenticate('signInWithPassword', email, password);
   }
